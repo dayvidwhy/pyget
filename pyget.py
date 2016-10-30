ffdd1ce4b25c7e620ba6015ed7fa502415974629
David Young <dayvidwhy@gmail.com>
2016-10-30 16:19:13 +1000

    Clean up some bits, suggest some examples to download

--- pyget.py
+++ pyget.py
@@ -1,3 +1,15 @@
+###################################################################
+##
+##   COMS3200 Assignment at UQ
+##   Must be able to retrieve files with
+##   ftp:// or http:// prefix.
+##
+##   Example files to grab:
+##   http://www.itee.uq.edu.au/filething/get/2646/phone-grey.png
+##   ftp://ftp.itee.uq.edu.au/welcome.msg
+##
+###################################################################
+
 import sys
 from ftplib import FTP
 from urllib.parse import urlparse
@@ -5,76 +17,78 @@ from urllib.request import urlopen
 from requests import get
 
 """
-	Go and get our file using the ftp library
-	provided by python3.
+    Go and get our file using the ftp library
+    provided by python3.
 """
 def getFtpFile(urlPased, fileGet):
-	#make ftp connection, set as passive by default
+	print('Getting your ftp file...')
+    #make ftp connection, set as passive by default
 	try:
 		ftp = FTP(urlParsed.netloc)
 	except Exception as e:
 		#server we tried to connect to didn't work
-		print("ERROR:> Cannot resolve \""+urlParsed.netloc+"\"")
+		print("Error: Cannot resolve \""+urlParsed.netloc+"\"")
 	try:
-		ftp.login('anonymous','coms3200@uq.edu.au')
-		print('Connected successfully...')
+		ftp.login('anonymous')
+		print("Connected successfully...")
 		#check to see if we need to find the sub dir
 		if (len(pathSections)>2):
 			print("Need to change directories...")
-			directsToFile = pathSections[1:-1]
-			#for each folder in the path to the file
-			for folder in directsToFile:
-				currentDir = []
-				print('Checking for directory '+folder)
-				ftp.retrlines('LIST', currentDir.append)
-				#is that folder a name of any of the files in this directory
-				if (any(folder in fileList for fileList in currentDir)):
+			subfolders = pathSections[1:-1]
+			# For each folder in the path to the file
+			for folder in subfolders:
+				current = []
+				print("Checking for directory " + folder)
+				ftp.retrlines('LIST', current.append)
+				# Is that folder a name of any of the files in this directory
+				if (any(folder in fileList for fileList in current)):
 					#found it, move in
 					ftp.cwd(folder)
-					print("212: Moved to directory "+folder)
+					print("212: Moved to directory " + folder)
 				else:
-					#does not exist
-					print("That directory doesnt exist!")
+					print("That directory doesnt exist, stopping!")
 					ftp.quit()
 					return
-		#populate a list with the contents of the current dir
+
+		# Populate a list with the contents of the current directory
 		files = []
 		ftp.retrlines('LIST', files.append)
-		#check to see if our file exists on the server
-		print('Checking if file exists...')
+		# Check to see if our file exists on the server
+		print("Checking if file exists...")
 		if (any(fileGet in s for s in files)):
-			print('Found '+fileGet+"!")
-			print('Downloading now...')
+			print("Found " + fileGet + "!")
+			print("Downloading now...")
 			ftp.retrbinary('RETR '+fileGet, open(fileGet, 'wb').write)
-			print('216: Successfully saved '+fileGet)
+			print("216: Successfully saved " + fileGet)
 			ftp.quit()
 		else:
-			#our file does not exist on the server
-			print('550: Requested action not taken. File unavailable, not found, not accessible')
+			# Trouble getting the file, might not be there, or permissions
+			print("550: Requested action not taken. File unavailable, not found, not accessible")
 			ftp.quit()
 	except Exception as e:
 		#catch any exception and print it out
-		print(str(e))
-	
+		print("Error: " + str(e))
+
 """
 	Using the requests library download an http based
 	file to our directory that the program is executed
 	from.
 """
 def getHttpFile(url, fileGet):
-	print('Getting your http file...')
-	print('Connecting...')
-	req = get(url)
-	print('Connected successfully...')
-	#open a file to write binary
-	fi = open(fileGet,'wb')
-	#write the contents of the url we connected to
-	fi.write(req.content)
-	print(fileGet + ' written successfully!')
+	print("Getting your http file...")
+	print("Connecting...")
+	try:
+		req = get(url)
+		print("Connected successfully...")
+		fi = open(fileGet,'wb')
+		fi.write(req.content)
+		print(fileGet + " was written successfully!")
+	except Exception as e:
+		print("Could not connect, check the url.")
 
 """
-	Execute our functions depending on whether we are
-	dealing with an ftp or http based connection.
+    Execute our functions depending on whether we are
+    dealing with an ftp or http based connection.
 """
 if __name__ == "__main__":
 	#extract the arg passed to program
@@ -90,4 +104,4 @@ if __name__ == "__main__":
 	elif (urlParsed.scheme == 'ftp'):
 		getFtpFile(urlParsed, fileGet)
 	else:
-		print('i dont understand that protocol')
+		print("Usage: python3[.5] pyget.py ftp://[path_to_file] | http://[path_to_file]")
